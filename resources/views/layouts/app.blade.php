e64d9459603b99d4deb3b6501f16684b997840c5
Afifa Sultana <afifasultana637@gmail.com>
2025-05-02 23:33:01 +0600

    blade files added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,5 @@
 <x-base-layout :title="$title">
     <x-layouts.header />
     {{ $slot }}
-    <footer>
-        
-            <a href = "#">Link1</a>
-            <a href = "#">Link2</a>
-            {{ $footerLinks }}
-    </footer>
 </x-base-layout>
     
